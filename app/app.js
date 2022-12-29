63df4c15a294eb0ecebbcba64b159409dac19666
DeveloperDTH <scratchdth@gmail.com>
2022-12-29 20:43:21 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -3,11 +3,13 @@
 const express = require("express");
 const app = express();
 //라우팅
-const home = require("../routes/home");
+const home = require("./src/routes/home");
 
 
-app.set("views","./views");
+app.set("views","./src/views");
 app.set("view engine","ejs");
+
+app.use(express.static(`${__dirname}/src/public`))
 
 app.use("/",home);
 
