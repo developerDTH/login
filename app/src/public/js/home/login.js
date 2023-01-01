b35201b66be02d0926006f1ce30cffb8c70499c1
DeveloperDTH <scratchdth@gmail.com>
2023-01-01 18:41:36 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -19,8 +19,16 @@ function login(){
             "Content-Type":"application/json",
         },
         body:JSON.stringify(req)            
-    }).then((res)=> res.json())
+    })
+    .then((res)=> res.json())
     .then((res)=>{
-
-    });
+        if(res.success){
+            location.href = "/";
+        }else{
+            alert(res.msg);
+        }
+    })
+    .catch((err)=>{
+        console.err(new Error("로그인중 에러발생"));
+    })
 }
