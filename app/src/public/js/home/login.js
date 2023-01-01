b638c813b50e209c04a727d2e875867bab68d176
DeveloperDTH <scratchdth@gmail.com>
2023-01-01 15:10:23 +0900

    로그인요청하는 fetch만듬

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { json } = require("express");
+
 const id = document.querySelector("#id"),
 passwd = document.querySelector("#passwd"),
 loginbtn = document.querySelector("button");
@@ -11,5 +13,11 @@ function login(){
         id:id.value,
         passwd:passwd.value
     };
-    console.log(req)
+    fetch("/login",{
+        method : "POST",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body:JSON.stringify(req)            
+    });
 }
