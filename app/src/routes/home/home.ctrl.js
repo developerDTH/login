ac6b5a18cad5ae8ac36839ecdf7cc17812a70abe
DeveloperDTH <scratchdth@gmail.com>
2023-01-03 22:06:14 +0900

    유저모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/user");
+
 
 const output = {
     home : (req, res)=>{
@@ -12,21 +13,11 @@ const output = {
 }
 const process = {
     login:(req,res)=>{
-        const id = req.body.id,
-        passwd = req.body.passwd;
-
-        const users = UserStorage.getUsers("id","passwd");
-        const response = {};
-        if (users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.passwd[idx]===passwd){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다.";
+        const user = new User(req.body);
+        const response = user.login()
+        
         return res.json(response);
+        
     },
 };
 module.exports={
