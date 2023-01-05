480ea3228dbfcff01c9b969b09660b36e77479b2
DeveloperDTH <scratchdth@gmail.com>
2023-01-05 12:50:34 +0900

    fs모듈을 이용한 로그인기능 구현

--- app/src/models/userStorage.js
+++ app/src/models/userStorage.js
@@ -1,13 +1,19 @@
 "use strict"
 
+const fs = require("fs").promises;
 class UserStorage{
-    static #users={
-        id : ["scratchDTH","DeveloperDTH","scratchDTH2"],
-        passwd:["shy030902*","DeveloperDTH0202!","ush030902!"],
-        name:["승호","승호","승호"]
-    };
+    static #getUserInfo(){
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users);
+        const userInfo = userKeys.reduce((newUser, info)=>{
+                newUser[info] = users[info][idx]
+                return newUser;
+                },{});
+                return userInfo;
+    }    
     static getUsers(...fields){
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers,field)=>{
             if (users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -18,17 +24,15 @@ class UserStorage{
         return newUsers;
     }
     static getUserInfo(id){
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const userKeys = Object.keys(users);
-        const userInfo = userKeys.reduce((newUser, info)=>{
-            newUser[info] = users[info][idx]
-            return newUser;
-        },{});
-        return userInfo;
+        return fs.readFile("./src/databases/users.json")
+            .then((data)=>{
+                return this.#getUserInfo()
+            })
+            .catch((err)=> console.error);
     }
+    
     static save(userInfo){
-        const user = this.#users;
+        //const user = this.#users;
         user.id.push(userInfo.id);
         user.name.push(userInfo.name);
         user.passwd.push(userInfo.passwd);
